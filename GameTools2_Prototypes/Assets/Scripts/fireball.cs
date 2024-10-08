41daf9902f74ae3591d4c0d290cd522ae1220ddb
kb478 <c23789389@mytudublin.ie>
2024-10-08 11:57:57 +0100

    Unity Particle Pack
    half the shits broken, might remove

--- GameTools2_Prototypes/Assets/Scripts/fireball.cs
+++ GameTools2_Prototypes/Assets/Scripts/fireball.cs
@@ -1,16 +1,34 @@
+using System;
 using UnityEngine;
 
 public class fireball : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Explosion Effect")] 
+    public LayerMask hit_Layer;
+    public LayerMask block_Layer;
+    public int max_Hits;
+    public int max_Damage;
+    public int min_Damage;
+    public float radius;
+    public float explosion_Force;
+
+    private Collider[] hit_Colliders;
+
+    private void Awake()
     {
-        
+        hit_Colliders = new Collider[max_Hits];
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
+        print("fireball exploding");
         
+        if (other.CompareTag("Player"))
+        {
+            Physics.Raycast()
+            int hits = Physics.OverlapSphereNonAlloc(other.transform.position, radius, hit_Colliders, hit_Layer);
+        }
+        
+        Destroy(gameObject);
     }
 }
